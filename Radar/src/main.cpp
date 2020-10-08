3b996c54fc7d9e1e6602afe2f8fb9c996b3fec31
oskarblo02 <54669844+oskarblo02@users.noreply.github.com>
2020-10-08 13:11:45 +0200

    Update main.cpp

--- Radar/src/main.cpp
+++ Radar/src/main.cpp
@@ -1,16 +1,34 @@
 #include <Arduino.h>
 #include <NewPing.h>
+#include <Servo.h>
 
-NewPing sonar (10, 11, 500);
+Servo name_servo;
 
+NewPing sonar (10, 11, 500); // 10: trig_pin 11: echo_pin 500: distance_cm
+
+int servo_position = 0;
 
 void setup() {
   Serial.begin(9600);
+  name_servo.attach (9);
   delay(50);
 }
 
 void loop() {
-  Serial.print("avstånd:");
-  Serial.println(sonar.ping_cm());
-  delay(500);
+    for (servo_position =0; servo_position <=180; servo_position +=2){
+    name_servo.write(servo_position);
+    Serial.print("grader: ");
+    Serial.println(servo_position); 
+    Serial.print("avstånd: ");
+    Serial.println(sonar.ping_cm());
+    delay(50);
+  }
+  for (servo_position =180; servo_position >=0; servo_position -=2){
+    name_servo.write(servo_position);
+    Serial.print("grader: ");
+    Serial.println(servo_position); 
+    Serial.print("avstånd:");
+    Serial.println(sonar.ping_cm());
+    delay(50);
+  }
 }
